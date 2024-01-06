6906192c68e1f74043caa20753a6cfa52a9e83ac
trungndse150553 <trungndse150553@fpt.edu.vn>
2024-01-06 11:32:08 +0700

    Init Code Base

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
@@ -6,9 +7,25 @@ const hostname = 'localhost'
 const port = 8017
 
 app.get('/', (req, res) => {
-  res.end('<h1>Hello World</h1>')
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}/`)
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at http://${hostname}:${port}/`)
 })
