f50a8cdaa2619e91df6969a4bc24572ea8a36879
trungndse150553 <trungndse150553@fpt.edu.vn>
2024-01-13 11:27:51 +0700

    Validation with Joi

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
+import { boardValidation } from '~/validations/boardValidation'
 
 const Router = express.Router()
 
@@ -7,8 +8,6 @@ Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
   })
-  .post((req, res) => {
-    res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' })
-  })
+  .post(boardValidation.createNew)
 
 export const boardRoute = Router
