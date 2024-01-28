49ee99abb1c1e469973de8f4aae10392cb73648c
trungndse150553 <trungndse150553@fpt.edu.vn>
2024-01-28 09:52:51 +0700

    API drag Column

--- src/models/columnModel.js
+++ src/models/columnModel.js
@@ -58,7 +58,7 @@ const pushCardOrderIds = async (card) => {
         { returnDocument: 'after' }
       )
 
-    return result.value
+    return result
   } catch (error) {
     throw new Error(error)
   }
