564b42c5cf7cef9b18d9871e054da47056afaba1
trungndse150553 <trungndse150553@fpt.edu.vn>
2024-02-01 10:06:50 +0700

    Update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  // Không cần localhost nữa vì file config/cors đã luôn luôn cho phép môi trường dev
+  // 'http://localhost:5173'
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
