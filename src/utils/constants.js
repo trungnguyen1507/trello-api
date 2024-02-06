75e0163584d8484999b4f6cdb6284cc6be3685d4
trungndse150553 <trungndse150553@fpt.edu.vn>
2024-02-06 10:48:12 +0700

    Update CORS allow Front-end

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // Không cần localhost nữa vì file config/cors đã luôn luôn cho phép môi trường dev
   // 'http://localhost:5173'
+  'https://trello-web-sigma.vercel.app'
 ]
 
 export const BOARD_TYPES = {
