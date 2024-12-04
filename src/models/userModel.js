eb788362af3a86f4ddd056d9a67459ec48388dde
trungnguyen1507 <ndtrungpy@gmail.com>
2024-12-04 16:26:01 +0700

    Code API Register User

---
+++ src/models/userModel.js
@@ -0,0 +1,91 @@
+import Joi from 'joi'
+import { ObjectId } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+import { EMAIL_RULE, EMAIL_RULE_MESSAGE } from '~/utils/validators'
+
+const USER_ROLES = {
+  CLIENT: 'client',
+  ADMIN: 'admin'
+}
+
+const USER_COLLECTION_NAME = 'users'
+const USER_COLLECTION_SCHEMA = Joi.object({
+  email: Joi.string().required().pattern(EMAIL_RULE).message(EMAIL_RULE_MESSAGE),
+  password: Joi.string().required(),
+  // username cắt ra từ email sẽ có khả năng không unique bởi vì sẽ có những tên email trùng nhau nhưng từ các nhà cung cấp khác nhau
+  username: Joi.string().required().trim().strict(),
+  displayName: Joi.string().required().trim().strict(),
+  avatar: Joi.string().default(null),
+  role: Joi.string().valid(USER_ROLES.CLIENT, USER_ROLES.ADMIN).default(USER_ROLES.CLIENT),
+
+  isActive: Joi.boolean().default(false),
+  verifyToken: Joi.string(),
+
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+// Chỉ định ra những field mà chúng ta không muốn update
+const INVALID_UPDATE_FIELDS = ['_id', 'createdAt', 'username', 'email']
+
+const validateBeforeCreate = async (data) => {
+  return await USER_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+    const createdUser = await GET_DB().collection(USER_COLLECTION_NAME).insertOne(validData)
+    return createdUser
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneById = async (userId) => {
+  try {
+    const result = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .findOne({ _id: new ObjectId(userId) })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneByEmail = async (emailValue) => {
+  try {
+    const result = await GET_DB().collection(USER_COLLECTION_NAME).findOne({ email: emailValue })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const update = async (userId, updateData) => {
+  try {
+    // Lọc những dữ liệu không muốn update
+    Object.keys(updateData).forEach((fieldName) => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    })
+    const result = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .findOneAndUpdate({ _id: new ObjectId(userId) }, { $set: updateData }, { returnDocument: 'after' })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const userModel = {
+  USER_COLLECTION_NAME,
+  USER_COLLECTION_SCHEMA,
+  USER_ROLES,
+  createNew,
+  findOneById,
+  findOneByEmail,
+  update
+}
---
+++ src/services/userService.js
@@ -0,0 +1,38 @@
+import { StatusCodes } from 'http-status-codes'
+import { userModel } from '~/models/userModel'
+import ApiError from '~/utils/ApiError'
+import bcryptjs from 'bcryptjs'
+import { v4 as uuidv4 } from 'uuid'
+import { pickUser } from '~/utils/formatters'
+
+const createNew = async (reqBody) => {
+  try {
+    // Kiểm tra xem email đã tồn tại trong hệ thống hay chưa
+    const existUser = await userModel.findOneByEmail(reqBody.email)
+    if (existUser) {
+      throw new ApiError(StatusCodes.CONFLICT, 'Email already exists')
+    }
+    // Tạo data để lưu vào Database
+    const nameFromEmail = reqBody.email.split('@')[0]
+    const newUser = {
+      email: reqBody.email,
+      password: bcryptjs.hashSync(reqBody.password, 8),
+      username: nameFromEmail,
+      displayName: nameFromEmail,
+      verifyToken: uuidv4()
+    }
+    // Lưu thông tin user vào Database
+    const createdUser = await userModel.createNew(newUser)
+    const getNewUser = await userModel.findOneById(createdUser.insertedId)
+    // Gửi email cho người dùng xác thực tài khoản
+
+    // return dữ liệu cho Controller
+    return pickUser(getNewUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+export const userService = {
+  createNew
+}
